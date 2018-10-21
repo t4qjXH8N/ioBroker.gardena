687e3ed329716e36ae18aa38517a309c48a68bcd
Christian <chvorholt@gmail.com>
2018-10-21 23:36:19 +0200

    added missing translations

--- admin/words.js
+++ admin/words.js
@@ -3,45 +3,157 @@
 
 systemDictionary = {
     "gardena settings": {
-      "en": "Gardena smart system settings",
+      "en": "Gardena Smart System settings",
+      "de": "Gardena Smart Systemeinstellungen",
+      "ru": "Настройки смарт-системы Gardena",
+      "pt": "Configurações do Gardena Smart System",
+      "nl": "Gardena Smart System-instellingen",
+      "fr": "Paramètres du système intelligent Gardena",
+      "it": "Impostazioni di Gardena Smart System",
+      "es": "Configuración del sistema inteligente Gardena",
+      "pl": "Ustawienia inteligentnego systemu Gardena"
     },
-    "gardena_password": {
+    "gardena_password":  {
       "en": "Password",
+      "de": "Passwort",
+      "ru": "пароль",
+      "pt": "Senha",
+      "nl": "Wachtwoord",
+      "fr": "Mot de passe",
+      "it": "Parola d'ordine",
+      "es": "Contraseña",
+      "pl": "Hasło"
     },
     "gardena_polling_interval": {
-      "en": "Polling interval (s)"
+      "en": "Polling interval (s)",
+      "de": "Abrufintervall (s)",
+      "ru": "Интервал опроса (s)",
+      "pt": "Intervalo de pesquisa (s)",
+      "nl": "Polling-interval (s)",
+      "fr": "Intervalle d'interrogation (s)",
+      "it": "Intervallo di polling (s)",
+      "es": "Intervalo de sondeo (s)",
+      "pl": "Interwały sondowania (s)"
     },
     "gardena_reconnect_interval": {
-      "en": "Time between reconnection tries (s)",
+      "en": "Timeout for reconnect (s)",
+      "de": "Timeout für erneute Verbindung (s)",
+      "ru": "Тайм-аут для повторного подключения (s)",
+      "pt": "Tempo limite para reconectar (s)",
+      "nl": "Time-out voor opnieuw verbinden (s)",
+      "fr": "Délai de reconnexion (s)",
+      "it": "Timeout per ricollegare (s)",
+      "es": "Tiempo de espera para reconectar (s)",
+      "pl": "Czas oczekiwania na ponowne połączenie (s)"
     },
     "gardena_username": {
       "en": "Username",
+      "de": "Nutzername",
+      "ru": "имя пользователя",
+      "pt": "Nome de usuário",
+      "nl": "Gebruikersnaam",
+      "fr": "Nom d'utilisateur",
+      "it": "Nome utente",
+      "es": "Nombre de usuario",
+      "pl": "Nazwa Użytkownika"
     },
     "loading_message": {
-      "en": "Loading gardena states",
+      "en": "Loading datapoints from Gardena",
+      "de": "Datenpunkte von Gardena laden",
+      "ru": "Загрузка данных из Gardena",
+      "pt": "Carregando datapoints de Gardena",
+      "nl": "Datapunten laden van Gardena",
+      "fr": "Chargement de points de données depuis Gardena",
+      "it": "Caricamento dei datapoint da Gardena",
+      "es": "Cargando puntos de datos de Gardena",
+      "pl": "Ładowanie punktów danych z Gardeny"
     },
     "login_data": {
       "en": "Authentification",
+      "de": "Authentifizierung",
+      "ru": "Authentification",
+      "pt": "Autenticação",
+      "nl": "authentificatie",
+      "fr": "L'authentification",
+      "it": "autenticazione",
+      "es": "Autentificación",
+      "pl": "Uwierzytelnienie"
     },
     "no_connection_to_gardena": {
-      "en": "No connection to Gardena cloud",
+      "en": "No connection to Gardena web service",
+      "de": "Keine Verbindung zum Gardena Webservice",
+      "ru": "Нет подключения к веб-сервису Gardena",
+      "pt": "Nenhuma conexão com o serviço da Web Gardena",
+      "nl": "Geen verbinding met Gardena webservice",
+      "fr": "Pas de connexion au service Web Gardena",
+      "it": "Nessuna connessione al servizio web di Gardena",
+      "es": "Sin conexión al servicio web de Gardena.",
+      "pl": "Brak połączenia z serwisem internetowym Gardena"
     },
     "select_all": {
       "en": "Check all (not recommended)",
+      "de": "Alle anwählen (nicht empfohlen)",
+      "ru": "Проверить все (не рекомендуется)",
+      "pt": "Verifique tudo (não recomendado)",
+      "nl": "Controleer alles (niet aanbevolen)",
+      "fr": "Tout cocher (non recommandé)",
+      "it": "Controlla tutto (non consigliato)",
+      "es": "Marcar todo (no recomendado)",
+      "pl": "Sprawdź wszystkie (niezalecane)"
     },
-    "deselect_all": {
+    "deselect_all":  {
       "en": "Uncheck all",
+      "de": "Alle abwählen",
+      "ru": "Снять все",
+      "pt": "Desmarcar todos",
+      "nl": "Deselecteer alles",
+      "fr": "Décocher tout",
+      "it": "Deseleziona tutto",
+      "es": "Desmarcar todo",
+      "pl": "Odznacz wszystkie"
     },
     "gardena_smart_datapoints": {
       "en": "Smart datapoints",
+      "de": "Intelligente Datenpunkte",
+      "ru": "Интеллектуальные данные",
+      "pt": "Pontos de dados inteligentes",
+      "nl": "Slimme datapunten",
+      "fr": "Points de données intelligents",
+      "it": "Punti dati intelligenti",
+      "es": "Puntos de datos inteligentes",
+      "pl": "Inteligentne punkty danych"
     },
     "gardena_update_locations_counter": {
-      "en": "Update locations each poll",
+      "en": "Update of locations every x-th poll only",
+      "de": "Aktualisierung der Standorte nur bei jeder x-ten Abfrage",
+      "ru": "Обновление мест только для каждого x-го опроса",
+      "pt": "Atualização de locais apenas a cada x-enquete",
+      "nl": "Update van locaties elke x-de peiling alleen",
+      "fr": "Mise à jour des lieux chaque X-ème scrutin seulement",
+      "it": "Aggiornamento di sedi solo per il voto x-esimo",
+      "es": "Actualización de ubicaciones solo cada X-th encuesta",
+      "pl": "Aktualizacja lokalizacji co X-ty ankieta"
     },
     "on save adapter restarts with new config immediately": {
       "en": "on save adapter restarts with new config immediately",
+      "de": "Beim Speichern wird der Adapter sofort mit der neuen Konfiguration neu gestartet",
+      "ru": "на перезаписываемом адаптере перезагружается с новой конфигурацией немедленно",
+      "pt": "on save adapter reinicia com nova configuração imediatamente",
+      "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config",
+      "fr": "sur save, l'adaptateur redémarre immédiatement avec la nouvelle configuration",
+      "it": "su save adapter si riavvia immediatamente con la nuova configurazione",
+      "es": "en guardar adaptador se reinicia con nueva configuración inmediatamente",
+      "pl": "na karcie save natychmiast uruchamia się z nową konfiguracją"
     },
     "check_connection": {
-      "en": "check connection",
+      "en": "connection check",
+      "de": "Verbindungsprüfung",
+      "ru": "проверка связи",
+      "pt": "verificação de conexão",
+      "nl": "verbindingscontrole",
+      "fr": "vérification de la connexion",
+      "it": "controllo della connessione",
+      "es": "verificación de conexión",
+      "pl": "sprawdzanie połączenia"
     }
 };
